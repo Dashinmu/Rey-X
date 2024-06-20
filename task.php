59316a4c75354d2fe94cb25bf34fa2c7d0530ae6
Dashinmu <90204594+Dashinmu@users.noreply.github.com>
2024-06-20 22:32:11 +0500

    beta v0.99

--- task.php
+++ task.php
@@ -130,7 +130,7 @@ if ($usertype != 1 && $usertype != 2) {
                                             <span class = "task-answer-span"><?php echo $row['TASK_DESC'] ?></span>
                                         </div>
                                         <div class = "task-answer-area">
-                                            <textarea class = "textarea" placeholder = "Введите решение" value = "<?php echo $row['LAST_ANSWER'] ?>"></textarea>
+                                            <textarea class = "textarea" placeholder = "Введите решение" text = "<?php echo $row['LAST_ANSWER'] ?>"></textarea>
                                         </div>
                                         <button type="button" class="btn answer btn-primary btn-block <?php echo $status ?>">Отправить на проверку</button>
                                     </div>
