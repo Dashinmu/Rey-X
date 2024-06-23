7c8b426259f2450d4a9ea311ff1920672c3d55ed
Dashinmu <90204594+Dashinmu@users.noreply.github.com>
2024-06-23 22:26:11 +0500

    beta v1.00

--- task.php
+++ task.php
@@ -226,7 +226,7 @@ if ($usertype != 1 && $usertype != 2) {
                 </div>
             </div>
             <div class = "stages-info-block">
-                <div class = "stage-info-item" id = "stage-<?php echo $row['STAGE_ID']?>">
+                <div class = "stage-info-item">
                     <?php
                         $lastrow = 0;
                         while ($row = oci_fetch_array($all_stages_info, OCI_RETURN_NULLS + OCI_ASSOC)) {
