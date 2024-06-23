081b69b34d61e83145e0419603556e210114c8c8
Dashinmu <90204594+Dashinmu@users.noreply.github.com>
2024-06-23 21:32:44 +0500

    beta v1.00

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ if ($usertype != 1 && $usertype != 2) {
             ?>
             <div class="rating-ellipse">
                 <span class = "rating-ellipse3">
-                    <span class = "rating-ellipse2"><?php echo $p_current_rating?></span>
+                    <span class = "rating-ellipse2 <?php if ($p_current_rating < 4) { echo "bad"; }?>"><?php echo $p_current_rating?></span>
                     /<?php echo $p_max_rating?>
                 </span>
             </div>
