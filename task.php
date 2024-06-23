936c6b5676b62aa3ad8d09c965f99a8c3a9a5e22
Dashinmu <90204594+Dashinmu@users.noreply.github.com>
2024-06-23 22:34:18 +0500

    beta v1.00

--- task.php
+++ task.php
@@ -469,8 +469,8 @@ require_once "modal.php";
     $(function() {
         $(".stage-info-item .stage-items-info .item-info .item-descrip .task-answer").click(function() {
             task_id = Number(this.id.replace('task-', ''));
-            var stageid = $(this).closest(".stage-info-item");
-            var stage_id = Number(stageid.id.replace('stage-',''));
+            var stageid = $(this).closest(".stage-info-item").attr(id);
+            alert(stageid);
             $("#taskInfoModal").modal('toggle');
         })
     });
